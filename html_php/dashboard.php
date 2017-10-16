d0f352ed96cb3c7313d2f2893a9a86d41e119c14
fzhongjie <fzhongjie@msn.com>
2017-10-16 16:21:04 -0400

    Zhongjie updated.

--- html_php/dashboard.php
+++ html_php/dashboard.php
@@ -2,6 +2,7 @@
 session_start();
 ?>
 
+<!DOCTYPE html>
 <html lang="en" data-textdirection="ltr" class="loading">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
--- html_php/login.php
+++ html_php/login.php
@@ -38,7 +38,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 ?>
 
 
-
+<!DOCTYPE html>
 <html lang="en" data-textdirection="ltr" class="loading">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
