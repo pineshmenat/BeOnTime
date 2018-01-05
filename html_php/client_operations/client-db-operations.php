2157d8d34c71b4c327dc9978459bab75e3900fe6
vaishnavipanchal23 <vaishnavi.panchalk@gmail.com>
2018-01-05 15:13:52 -0500

    Changes Info: 1. removed AssignedBy and AssignedTo insert for shifts, as it will be taken care from manager side 2. created DB functions for delete Shift, update Star Rating and Client review, get DB records for Client side shifts retrieval on basis of location, designation, start & end time.

--- html_php/client_operations/client-db-operations.php
+++ html_php/client_operations/client-db-operations.php
@@ -1,6 +1,7 @@
 <?php
 require_once "../model/db_config.php";
 require_once "../model/CompanyLocation.php";
+require_once "../model/ShiftDetails.php";
 /**
  * Created by PhpStorm.
  * User: Vaishnavi
@@ -52,7 +53,7 @@ class ClientSideDB{
     {
         $designations = array();
         $db = DB::getDBConnection();
-        $sql = "SELECT empDesignationId, empDesignationName FROM employeeDesignationmaster";
+        $sql = "SELECT empDesignationId, empDesignationName FROM employeedesignationmaster";
         $pdoStmt = $db->prepare($sql);
         $pdoStmt->execute();
         $result = $pdoStmt->fetchAll();
@@ -66,17 +67,79 @@ class ClientSideDB{
     public static function insertShifts($employeeDesignationId, $companyId, $companyLocationId, $dateStartTime, $dateEndTime)
     {
         $db = Db::getDBConnection();
-        $sql = "INSERT INTO shiftmaster(empDesignationId, AssignedBy, AssignedTo, CompanyId, CompanyLocationId, StartTime, EndTime, ShiftStatus) "
-            ."VALUES (:employeeDesignationId, :assignedBy, :assignedTo, :companyId, :companyLocationId, :dateStartTime, :dateEndTime, :shiftStatus)";
+        $sql = "INSERT INTO shiftmaster(empDesignationId, CompanyId, CompanyLocationId, StartTime, EndTime, ShiftStatus) "
+            ."VALUES (:employeeDesignationId, :companyId, :companyLocationId, :dateStartTime, :dateEndTime, :shiftStatus)";
         $pdoStmt = $db->prepare($sql);
         $pdoStmt->bindValue(':employeeDesignationId', $employeeDesignationId);
-        $pdoStmt->bindValue(':assignedBy', 10001);
-        $pdoStmt->bindValue(':assignedTo', 10007);
         $pdoStmt->bindValue(':companyId', $companyId);
         $pdoStmt->bindValue(':companyLocationId', $companyLocationId);
         $pdoStmt->bindValue(':dateStartTime', $dateStartTime);
         $pdoStmt->bindValue(':dateEndTime', $dateEndTime);
         $pdoStmt->bindValue(':shiftStatus', 'N');
+        $pdoStmt->execute();
+        $pdoStmt->closeCursor();
+    }
+
+    public static function getAllShifts($companyId, $companyLocation, $jobDesignation, $startDate, $endDate)
+    {
+        $shifts = [];
+        $db = Db::getDBConnection();
+        $sql = "SELECT ShiftId, UserName, FirstName, LastName, empDesignationName, payPerHour, StartTime, EndTime, ShiftStatus, clm.Address, clm.City, clm.PostalCode"
+            ." FROM shiftmaster sm"
+            ." INNER JOIN companylocationmaster clm ON sm.CompanyLocationId = clm.CompanyLocationId"
+            ." LEFT JOIN usermaster um ON sm.AssignedTo = um.UserId"
+            ." INNER JOIN employeedesignationmaster edm ON sm.empDesignationId = edm.empDesignationId"
+            ." WHERE sm.CompanyId= :companyId";
+        if(!empty($companyLocation))    $sql = $sql." and sm.CompanyLocationId=:companyLocation";
+        if(!empty($jobDesignation))    $sql = $sql." and sm.empDesignationId=:jobDesignation";
+        if(!empty($startDate))    $sql = $sql." and date(sm.StartTime) >= STR_TO_DATE(:startDate,'%M %d,%Y')";
+        if(!empty($endDate))    $sql = $sql." and date(sm.EndTime) <= STR_TO_DATE(:endDate,'%M %d,%Y')";
+        $sql = $sql." ORDER BY StartTime";
+
+        $pdoStmt = $db->prepare($sql);
+        $pdoStmt->bindValue(':companyId', $companyId);
+        if(!empty($companyLocation))    $pdoStmt->bindValue(':companyLocation', $companyLocation);
+        if(!empty($jobDesignation))    $pdoStmt->bindValue(':jobDesignation', $jobDesignation);
+        if(!empty($startDate))    $pdoStmt->bindValue(':startDate', $startDate);
+        if(!empty($endDate))    $pdoStmt->bindValue(':endDate', $endDate);
+
+        $pdoStmt->execute();
+        $result = $pdoStmt->fetchAll();
+        foreach ($result as $s) {
+            $shift = new ShiftDetails();
+            $shift->setShiftId($s['ShiftId']);
+            $shift->setFirstname($s['FirstName']);
+            $shift->setLastName($s['LastName']);
+            $shift->setEmpDesignationName($s['empDesignationName']);
+            $shift->setPayPerhour($s['payPerHour']);
+            $shift->setStartTime($s['StartTime']);
+            $shift->setEndTime($s['EndTime']);
+            $shift->setShiftStatus($s['ShiftStatus']);
+            $shift->setShiftAddress($s['Address']);
+            $shift->setShiftCity($s['City']);
+            $shift->setShiftPostalCode($s['PostalCode']);
+            $shifts[] = $shift;
+        }
+        $pdoStmt->closeCursor();
+        return $shifts;
+    }
+    public static function updateClientRatingAndReviewForShift($shiftId, $rating, $clientReview)
+    {
+        $db = Db::getDBConnection();
+        $sql = "UPDATE shiftmaster SET StarRating=:starRating, ClientReview=:clientReview WHERE ShiftId=:shiftId";
+        $pdoStmt = $db->prepare($sql);
+        $pdoStmt->bindValue(':starRating', $rating);
+        $pdoStmt->bindValue(':clientReview', $clientReview);
+        $pdoStmt->bindValue(':shiftId', $shiftId);
+        $pdoStmt->execute();
+        $pdoStmt->closeCursor();
+    }
+    public static function deleteShift($shiftId)
+    {
+        $db = Db::getDBConnection();
+        $sql = "DELETE FROM shiftmaster WHERE ShiftId=:shiftId";
+        $pdoStmt = $db->prepare($sql);
+        $pdoStmt->bindValue(':shiftId', $shiftId);
         $pdoStmt->execute();
         $pdoStmt->closeCursor();
     }
